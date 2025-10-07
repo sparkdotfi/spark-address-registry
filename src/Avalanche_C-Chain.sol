a83a044f602eb12f0f0c44ad418e0da849b2029b
Dom <18601956+hacker-DOM@users.noreply.github.com>
2025-10-07 17:43:20 +0200

    feat: Add Avalanche C-Chain Spark Vault V2 Impl and SpUSDC (#58)

---
+++ src/Avalanche_C-Chain.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+library Avalanche_CChain {
+
+    /******************************************************************************************************************/
+    /*** Spark Vault V2 Addresses                                                                                   ***/
+    /******************************************************************************************************************/
+
+    address internal constant SPARK_VAULT_V2_IMPL   = 0xC2C0582D1cCe30449cF561C7b9C4D6d527547F12;
+    address internal constant SPARK_VAULT_V2_SPUSDC = 0x1b992302652A92611DCd5090D1Cb388C6377f455;
+
+}
+
